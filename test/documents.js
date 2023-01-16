28dbe4639ecef2e42efd6ca32af78938b60d926c
Marlena Bazan <marlena.bazan@hotmail.com>
2023-01-16 22:47:56 +0100

    More tests

--- test/documents.js
+++ test/documents.js
@@ -51,5 +51,89 @@ describe('Documents', () => {
         });
     });
 
+    describe('GET /invalidpath', () => {
+        it('404 Invalid path', (done) => {
+            chai.request(server)
+                .get("/invalidpath")
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.have.property("errors");
+
+                    done();
+                });
+        });
+    });
+
+    describe('POST /docs', () => {
+        it('201 Create new document', (done) => {
+            let newDoc = {
+                title: 'test doc',
+                text: 'test'
+            }
+            chai.request(server)
+                .post("/docs")
+                .send(newDoc)
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.should.be.an("object");
+                    res.body.should.have.property("data");
+                    res.body.data.should.have.property("title");
+                    res.body.data.should.have.property("text");
+                    res.body.data.title.should.equal("test doc");
+                    res.body.data.text.should.equal("test");
+
+                    done();
+                });
+        });
+    });
+
+    describe('PUT /docs', () => {
+        it('201 Update a document', (done) => {
+            let updatedDoc = {
+                title: 'test doc',
+                text: 'test updated'
+            }
+            chai.request(server)
+                .put("/docs")
+                .send(updatedDoc)
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.should.be.an("object");
+                    res.body.should.have.property("data");
+                    res.body.data.result[0].should.have.property("title");
+                    res.body.data.result[0].should.have.property("text");
+                    res.body.data.result[0].title.should.equal("test doc");
+                    res.body.data.result[0].text.should.equal("test updated");
+    
+                    done();
+                });
+        });
+    });
+    
+    describe('DELETE /docs', () => {
+        it('204 Delete a document', (done) => {
+            chai.request(server)
+                .delete("/docs")
+                .end((err, res) => {
+                    res.should.have.status(204);
+    
+                    done();
+                });
+        });
+    });
+    
+    describe('GET /docs/:id', () => {
+        it('200 Get a document by id', (done) => {
+            chai.request(server)
+                .get("/docs/123")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.an("object");
+                    res.body.should.have.property("data");
+    
+                    done();
+                });
+        });
+    });
 });
 
