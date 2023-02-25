3437320da974f18c19de9aa860c43f1e34c35bec
Marlena Bazan <marlena.bazan@hotmail.com>
2023-02-25 01:32:03 +0100

    Added tests

--- test/documents.js
+++ test/documents.js
@@ -11,7 +11,19 @@ const server = require('../app.js');
 chai.should();
 chai.use(chaiHttp);
 
+const sinon = require('sinon');
+
+const expect = chai.expect;
+const Mailgun = require('mailgun.js');
+const formData = require('form-data');
+
+const inviteModel = require("../models/invite");
+
 describe('Documents', () => {
+    let user;
+    let token;
+    let doc;
+
     before(() => {
         return new Promise(async (resolve) => {
             const db = await database.getDb(collectionName);
@@ -35,16 +47,113 @@ describe('Documents', () => {
         });
     });
 
+    describe('POST /register', () => {
+        it('Register new user', (done) => {
+
+            const registerUser = {
+                email: `testemail@test.se`,
+                password: "testpassword"
+            };
+
+            chai.request(server)
+                .post("/auth/register")
+                .send(registerUser)
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.data.message.should.equal("User succesfully created");
+
+                    user = registerUser;
+
+                    done();
+                });
+        });
+
+        it('Register with no password', (done) => {
+
+            const registerUserWithNoPassword = {
+                email: `testemail1@test.se`,
+                password: ""
+            };
+
+            chai.request(server)
+                .post("/auth/register")
+                .send(registerUserWithNoPassword)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.errors.message.should.equal("E-mail or password missing");
+
+                    done();
+                });
+        });
+
+        it('Register with wrong email form', (done) => {
+
+            const registerUserWithNoPassword = {
+                email: `testwrongemail.se`,
+                password: "testpassword"
+            };
+
+            chai.request(server)
+                .post("/auth/register")
+                .send(registerUserWithNoPassword)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.errors.message.should.equal("E-mail is not in correct form");
+
+                    done();
+                });
+        });
+    });
+
+    describe('POST /login', () => {
+        it('Login', (done) => {
+
+            const loginUser = {
+                email: user.email,
+                password: user.password
+            };
+
+            chai.request(server)
+                .post("/auth/login")
+                .send(loginUser)
+                .end((err, res) => {
+                    res.should.have.status(201);
+
+                    token = res.body.data.token;
+
+                    done();
+                });
+        });
+
+        it('Login with wrong password', (done) => {
+
+            const loginUserWithWrongPassword = {
+                email: user.email,
+                password: "wrongpassword"
+            };
+
+            chai.request(server)
+                .post("/auth/login")
+                .send(loginUserWithWrongPassword)
+                .end((err, res) => {
+                    res.should.have.status(401);
+                    res.body.errors.message.should.equal("Password not correct");
+
+                    done();
+                });
+        });
+    });
 
     describe('GET /docs', () => {
         it('200 HAPPY PATH', (done) => {
             chai.request(server)
                 .get("/docs")
+                .set('x-access-token', token)
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.an("object");
                     res.body.data.should.be.an("array");
-                    res.body.data.length.should.be.equal(0);
+                    res.body.data.length.should.be.equal(1); // 1 = registered user above
 
                     done();
                 });
@@ -82,34 +191,82 @@ describe('Documents', () => {
                     res.body.data.title.should.equal("test doc");
                     res.body.data.text.should.equal("test");
 
+                    doc = res.body.data;
+
                     done();
                 });
         });
     });
 
-    describe('PUT /docs', () => {
+    describe('Root Query', () => {
+        it('returns a list of all documents', (done) => {
+            const query = `
+                query {
+                    documents {
+                        _id
+                        title
+                        text
+                    }
+                }
+            `;
+          
+          chai.request(server)
+            .post('/graphql')
+            .send({ query })
+            .end((err, res) => {
+                res.should.have.status(200);
+
+                done();
+            });
+        });
+
+        it('returns a single document by id', (done) => {
+            const query = `
+                query {
+                document(id: "${doc._id}") {
+                    _id
+                    title
+                    text
+                    }
+                }
+            `;
+            
+            chai.request(server)
+                .post('/graphql')
+                .send({ query })
+                .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an("object");
+                res.body.should.have.property("data");
+
+                done();
+            });
+        });
+    });
+
+    describe('PUT /docs/update', () => {
         it('201 Update a document', (done) => {
             let updatedDoc = {
-                title: 'test doc',
+                _id: doc._id,
                 text: 'test updated'
             }
             chai.request(server)
-                .put("/docs")
+                .put("/docs/update")
                 .send(updatedDoc)
                 .end((err, res) => {
-                    res.should.have.status(201);
+                    res.should.have.status(200);
                     res.body.should.be.an("object");
                     res.body.should.have.property("data");
-                    res.body.data.result[0].should.have.property("title");
-                    res.body.data.result[0].should.have.property("text");
-                    res.body.data.result[0].title.should.equal("test doc");
-                    res.body.data.result[0].text.should.equal("test updated");
+                    res.body.data.result.should.have.property("title");
+                    res.body.data.result.should.have.property("text");
+                    res.body.data.result.title.should.equal("test doc");
+                    res.body.data.result.text.should.equal("test updated");
     
                     done();
                 });
         });
     });
-    
+
     describe('DELETE /docs', () => {
         it('204 Delete a document', (done) => {
             chai.request(server)
@@ -135,5 +292,27 @@ describe('Documents', () => {
                 });
         });
     });
-});
 
+    describe('POST /invite', () => {
+        it('should call send function', () => {
+
+            const req = { 
+                body: { 
+                    recipient: 'test@test.com',
+                    title: 'Test'
+                }
+            };
+
+            const res = { 
+                status: sinon.spy(), 
+                json: sinon.spy() 
+              };
+              
+            const sendSpy = sinon.spy(inviteModel, 'send');
+            
+            inviteModel.send(req, res);
+        
+            expect(sendSpy.calledOnce).to.be.true;
+        });
+      });
+});
